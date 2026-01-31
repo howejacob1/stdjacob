f9a29b8ae032e340e8447b2b1efbfa6ffba7c038
Jacob Howe <howejacob1@gmail.com>
2026-01-31 15:40:42 +0000

    Rename INITIAL_TOKEN_CAPACITY to WER_INITIAL_TOKEN_CAPACITY

--- wer.c
+++ wer.c
@@ -6,18 +6,18 @@
 #include <ctype.h>
 
 // Helper to free token array
-static void free_wer_tokens(char** tokens, int count) {
-    FORTO(i, (uint)count) {
+static void free_wer_tokens(char** tokens, uint count) {
+    FORTO(i, count) {
         free(tokens[i]);
     }
     free(tokens);
 }
 
-#define INITIAL_TOKEN_CAPACITY 16
+#define WER_INITIAL_TOKEN_CAPACITY 16
 
 // Tokenize string into array of words
 // Returns array of strings, sets *count to number of tokens
-static char** to_wer_tokens(const char* text, int* count) {
+static char** to_wer_tokens(const char* text, uint* count) {
     *count = 0;
     // Make a copy to modify
     char* s = strdup_or_die(text);
@@ -27,7 +27,7 @@ static char** to_wer_tokens(const char* text, int* count) {
     remove_str_punctuation(s);
 
     // First pass: count tokens? No, just realloc.
-    int capacity = INITIAL_TOKEN_CAPACITY;
+    uint capacity = WER_INITIAL_TOKEN_CAPACITY;
     char** tokens = (char**)malloc_or_die(capacity * sizeof(char*));
     
     char* saveptr;
@@ -46,10 +46,10 @@ static char** to_wer_tokens(const char* text, int* count) {
 }
 
 double wer(const char* str_to_analyze, const char* what_str_should_be) {
-    int n_hyp = 0;
+    uint n_hyp = 0;
     char** hyp_tokens = to_wer_tokens(str_to_analyze, &n_hyp);
     
-    int n_ref = 0;
+    uint n_ref = 0;
     char** ref_tokens = to_wer_tokens(what_str_should_be, &n_ref);
     
     if (n_ref == 0) {
@@ -61,29 +61,34 @@ double wer(const char* str_to_analyze, const char* what_str_should_be) {
     // DP Table: (n_ref + 1) x (n_hyp + 1)
     // d[i][j] where i is ref index (0..n_ref), j is hyp index (0..n_hyp)
     
-    int rows = n_ref + 1;
-    int cols = n_hyp + 1;
+    uint rows = n_ref + 1;
+    uint cols = n_hyp + 1;
     
     // Allocate flat array
-    int* d = (int*)malloc_or_die((size_t)(rows * cols) * sizeof(int));
+    uint* d = (uint*)malloc_or_die((size_t)(rows * cols) * sizeof(uint));
     
     // Access macro: d[i][j] -> d[i * cols + j]
     #define D(i, j) d[(i) * cols + (j)]
     
     // Initialize
-    FORTO(i, (uint)rows) D(i, 0) = i; // Deletions
-    FORTO(j, (uint)cols) D(0, j) = j; // Insertions
+    FORTO(i, rows) D(i, 0) = i; // Deletions
+    FORTO(j, cols) D(0, j) = j; // Insertions
     
-    for (int i = 1; i <= n_ref; i++) {
-        for (int j = 1; j <= n_hyp; j++) {
+    for (uint i = 1; i <= n_ref; i++) {
+        for (uint j = 1; j <= n_hyp; j++) {
             // ref_tokens[i-1] vs hyp_tokens[j-1]
             if (streq(ref_tokens[i-1], hyp_tokens[j-1])) {
                 D(i, j) = D(i-1, j-1);
             } else {
-                int sub = D(i-1, j-1) + 1;
-                int ins = D(i, j-1) + 1;
-                int del = D(i-1, j) + 1;
-                D(i, j) = min3(sub, ins, del);
+                uint sub = D(i-1, j-1) + 1;
+                uint ins = D(i, j-1) + 1;
+                uint del = D(i-1, j) + 1;
+                // min3 takes ints, but we have uints. 
+                // Since these are small counts, casting to int is safe for min3.
+                // Or we should update min3 to take uints? 
+                // stdjacob.h defines min3(int, int, int).
+                // Let's cast to int for min3, then assign back to uint.
+                D(i, j) = (uint)min3((int)sub, (int)ins, (int)del);
             }
         }
     }
